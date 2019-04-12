b86af2204d9e7ee14d2290f9272271b5605893c4
Ivan Rozinko <ivanrozya@gmail.com>
2019-04-12 15:12:39 +0300

    Changed validation type

--- PS2/warmup/script.js
+++ PS2/warmup/script.js
@@ -1,49 +1,84 @@
 //Sum of numbers alignment
 const countSum = document.getElementById("countSum");
 countSum.addEventListener("click", () => {
-    const number1 = +document.getElementById("number1").value;
-    const number2 = +document.getElementById("number2").value;
-    if (number1 >= number2 || number1 < -100 || number2 > 100) {
-        alert("Проверьте диапазон")
+    const numberInput1 = document.getElementById("number1");
+    const numberInput2 = document.getElementById("number2");
+    if (!isValidInput(/^[-]*[\d]+$/, numberInput1)
+        || !isValidInput(/^[-]*[\d]+$/, numberInput2)) {
+        document.getElementById("result").innerHTML = "Недопустимый формат";
     } else {
-        let result = 0;
-        for (let i = number1; i <= number2; i++) {
-            if (i % 10 === 2 || i % 10 === 3 || i % 10 === 7) {
-                result += i;
+        const number1 = +numberInput1.value;
+        const number2 = +numberInput2.value;
+        if (number1 >= number2 || number1 < -100 || number2 > 100) {
+            alert("Проверьте диапазон")
+        } else {
+            let result = 0;
+            for (let i = number1; i <= number2; i++) {
+                if (i % 10 === 2 || i % 10 === 3 || i % 10 === 7) {
+                    result += i;
+                }
             }
+            document.getElementById("result").innerHTML = result.toString();
         }
-        document.getElementById("result").innerHTML = result.toString();
     }
 });
 
+/**
+ * Checking is parameter input matches to parameter regExp, and if not - changing
+ * input border colour to red
+ * @param regExp
+ * @param input
+ * @returns {boolean}
+ */
+
+function isValidInput(regExp, input) {
+    const isValidInput = regExp.test(input.value);
+    if (isValidInput) {
+        input.style.borderColor = "";
+        return true;
+    }
+    input.style.borderColor = "red";
+    return false;
+}
 
 //Time converter alignment
 const secConvertToTime = document.getElementById("secConvertToTime");
 secConvertToTime.addEventListener("click", () => {
-    let sec = parseInt(document.getElementById("timeSec").value);
-    let h, m, s;
-    s = sec % 60;
-    sec = Math.floor(sec / 60);
-    m = sec % 60;
-    sec = Math.floor(sec / 60);
-    h = sec % 60;
-    if (s < 10) {
-        s = "0" + s;
+    const timeSecInput = document.getElementById("timeSec");
+    if (!isValidInput(/^[-]*[\d]+$/, timeSecInput)) {
+        document.getElementById("result1").innerHTML = "Недопустимый формат";
+    } else {
+        let sec = +timeSecInput.value;
+        let h, m, s;
+        s = sec % 60;
+        sec = Math.floor(sec / 60);
+        m = sec % 60;
+        sec = Math.floor(sec / 60);
+        h = sec % 60;
+        if (s < 10) {
+            s = "0" + s;
+        }
+        if (m < 10) {
+            m = "0" + m;
+        }
+        if (h < 10) {
+            h = "0" + h;
+        }
+        document.getElementById("result1").innerHTML = h + ":" + m + ":" + s;
     }
-    if (m < 10) {
-        m = "0" + m;
-    }
-    if (h < 10) {
-        h = "0" + h;
-    }
-    document.getElementById("result1").innerHTML = h + ":" + m + ":" + s;
 });
 
 const timeConvertToSec = document.getElementById("timeConvertToSec");
 timeConvertToSec.addEventListener("click", () => {
-    const time = document.getElementById("time").value;
-    const t = time.split(":");
-    document.getElementById("result2").innerHTML = (+t[0]) * 3600 + (+t[1]) * 60 + (+t[2]);
+    const timeInput = document.getElementById("time");
+    if (!isValidInput(/^\d*:[0-5]\d:[0-5]\d$/, timeInput)) {
+        document.getElementById("result2").innerHTML = "Недопустимый формат";
+    } else {
+        const t = timeInput.value.split(":");
+        document.getElementById("result2").innerHTML = (+t[0]) * 3600
+            + (+t[1]) * 60
+            + (+t[2]);
+    }
 });
 
 //Time between 2 dates alignment
@@ -78,19 +113,25 @@ document.getElementById("clearBoard").addEventListener("click", () => {
 
 document.getElementById("drawBoard").addEventListener("click", () => {
     table.innerHTML = "";
-    const dimension = (document.getElementById("chessSize").value).split("x");
-    const chessWidth = dimension[0];
-    const chessHeight = dimension[1];
-    chessContainer.style.width = chessWidth * 22 + "px";
-    for (let i = 0; i < chessHeight; i++) {
-        for (let j = 0; j < chessWidth; j++) {
-            let block = document.createElement("div");
-            if (i % 2 === j % 2) {
-                table.appendChild(block);
-                block.className = "chessBlockWhite";
-            } else {
-                table.appendChild(block);
-                block.className = "chessBlockBlack";
+    const sizeInput = document.getElementById("chessSize");
+
+    if (!isValidInput(/^\d+[XxХх]\d+$/, sizeInput)) {
+        table.innerHTML = "Недопустимый формат";
+    } else {
+        const dimensions = sizeInput.value.split(/[XxХх]/);
+        const chessWidth = dimensions[0];
+        const chessHeight = dimensions[1];
+        chessContainer.style.width = chessWidth * 22 + "px";
+        for (let i = 0; i < chessHeight; i++) {
+            for (let j = 0; j < chessWidth; j++) {
+                let block = document.createElement("div");
+                if (i % 2 === j % 2) {
+                    table.appendChild(block);
+                    block.className = "chessBlockWhite";
+                } else {
+                    table.appendChild(block);
+                    block.className = "chessBlockBlack";
+                }
             }
         }
     }
@@ -109,14 +150,14 @@ textarea.addEventListener("focusout", (event) => {
     event.target.style.background = "";
     let string = textarea.value;
     let links = string.match(/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/g);
-    let iPs = string.match(/([0-9]{1,3}\.){3}[0-9]{1,3}/g);
+    let iPs = string.match(/(\d{1,3}\.){3}\d{1,3}/g);
     links.push.apply(links, iPs);
     const linksIPs = links.map((each) => each.replace(/https?:\/\//, ""));
     linksIPs.sort();
     document.getElementById("links").innerHTML = "";
     linksIPs.forEach((each) => {
         let link = document.createElement("a");
-        link.setAttribute("href", "_blank");
+        link.setAttribute("href", each);
         link.innerHTML = each;
         document.getElementById("links").appendChild(link);
     });
@@ -128,7 +169,7 @@ textarea.addEventListener("focusout", (event) => {
 
 
 //Textarea with RegExp
-let textInput = document.getElementById("reg-textarea");
+const textInput = document.getElementById("reg-textarea");
 const regexInput = document.getElementById("reg-input");
 const regButton = document.getElementById("reg-button");
 regButton.addEventListener("click", () => {
