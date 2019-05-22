c31904ea1233c81da8a1064a58bdd958c4c79fda
Rozinko Ivan <ivanrozya@gmail.com>
2019-05-22 14:56:51 +0300

    Finished with voting

--- PS4/voting/result.php
+++ PS4/voting/result.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Result</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
@@ -15,32 +16,28 @@
 
 
         function drawChart() {
-            const data = new google.visualization.DataTable();
+            let data = new google.visualization.DataTable();
             data.addColumn("string", "Breed");
             data.addColumn("number", "Rating");
 
+
             let breeds = [];
-            $.getJSON("json/results.json", function(jsonData){
+
+            $.getJSON("json/results.json", function (jsonData) {
                 $.each(jsonData, function (key, val) {
-                    // data.addRows(key, val);
-                    breeds.push(key )
-                    console.log(key + " " + val);
+                    breeds.push([key, val]);
                 });
 
+                data.addRows(breeds);
+
+                var options = {
+                    "title": "Most popular breeds",
+                    "width": 700,
+                    "height": 900
+                };
+                const chart = new google.visualization.PieChart(document.getElementById("chart_div"));
+                chart.draw(data, options);
             });
-
-            //https://stackoverflow.com/questions/4825899/how-to-push-both-key-and-value-into-an-array-in-jquery
-
-            var options = {
-                "title": "Most popular breeds",
-                "width": 500,
-                "height": 600
-            };
-
-            const chart = new google.visualization.PieChart(document.getElementById("chart_div"));
-            chart.draw(data, options);
-
-
         }
     </script>
 </head>
