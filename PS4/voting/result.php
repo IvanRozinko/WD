94a59718fde9f6ac8340c87e62061c9d131e6bde
Rozinko Ivan <ivanrozya@gmail.com>
2019-06-13 17:56:53 +0300

    Implement session_id(0 in code to give possibility to user to vote only once

--- PS4/voting/result.php
+++ PS4/voting/result.php
@@ -11,12 +11,18 @@
 <body>
 <?php
 session_start();
- session_id();
+session_regenerate_id();
+$total_votes = 0;
+ $_SESSION["total_votes"];
 
 if (isset($_POST["submit"])) {
 
-    if (isset($_POST["breed"]) && isNotVoted(session_id()) ) {
+    if (isset($_POST["breed"]) && hasNotVoted(session_id()) ) {
         writeToJSOn($_POST["breed"]);
+//        header("Location: result.php");
+    }
+    else {
+        echo "You are trying to cheat!";
     }
 }
 /**
@@ -37,15 +43,26 @@ function writeToJSON($breed)
     file_put_contents($file, $json_object);
 }
 
-function isNotVoted($id)
+function hasNotVoted($id)
 {
+
     $file = "json/voted.json";
     if (!file_exists($file)) {
-        file_put_contents($file); //here continue
+        file_put_contents($file, json_encode(array($_SESSION["total_votes"]++ => $id)));
+
+        return true;
     }
     $json_object = file_get_contents($file);
-    $IDs = json_decode($json_object);
-    return in_array($id, $IDs);
+    $IDs = json_decode($json_object, true);
+    print_r($IDs);
+    echo $_SESSION["total_votes"];
+
+    if (in_array($id, $IDs)) {
+        return false;
+    }
+   $IDs[$_SESSION["total_votes"]++] = $id;
+    file_put_contents($file, json_encode($IDs));
+    return true;
 }
 
 ?>
