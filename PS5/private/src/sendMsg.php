0d43fa9f8b9e888c603eec0f36434421f3d48eaa
Rozinko Ivan <ivanrozya@gmail.com>
2019-06-20 09:22:35 +0300

    Correction of last commit added src/ php files and .htaccess

---
+++ PS5/private/src/sendMsg.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+if ($_SESSION['session_id'] !== session_id()) {
+    header('Location: error.php');
+}
+if (isset($_POST["send_time"])) {
+    $name = $_SESSION["user_name"];
+    $time = $_POST["send_time"];
+    $input = $_POST["input"];
+    $path = "../msg/history.json";
+
+    $temp_array = json_decode(file_get_contents($path));
+    $msg = array (
+        "time" => $time,
+        "from" => $name . ": ",
+        "input" => $input
+    );
+    $temp_array[] = $msg;
+    //saving message to database file - "msg/history.json"
+    file_put_contents($path, json_encode($temp_array));
+    //sending message to user site
+    echo json_encode($msg);
+}
+
