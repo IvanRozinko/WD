1c680ed33162e542eff482dec4dbbd5355c25538
Ivan Rozinko <ivanrozya@gmail.com>
2019-09-05 22:15:32 +0300

    Added shape to balloon, made it editable, added key handling

--- ST3_Ballons/script.js
+++ ST3_Ballons/script.js
@@ -4,13 +4,15 @@ document.addEventListener('dblclick', createBalloon);
 
 
 function createBalloon(e) {
-  const main = document.getElementById('container');
+  const main = document.getElementById('container');  //TODO: can refactor all code. Create object balloon with X,Y, text and so on...
   const balloon = document.createElement('div');
   const label = document.createElement('p');
+  const offsetX = 30;
+  const offsetY = 110;
 
 
-  if (e.target.className === 'balloon' || e.target.className === 'label') {
-     return;
+  if (e.target.id !== 'container'){
+    return;
   }
 
   balloon.addEventListener('dblclick', addInput);
@@ -18,8 +20,8 @@ function createBalloon(e) {
   label.classList.add('label');
 
   balloon.classList.add('balloon');
-  balloon.style.left = e.pageX + 'px';
-  balloon.style.top = e.pageY + 'px';
+  balloon.style.left = e.pageX - offsetX + 'px';
+  balloon.style.top = e.pageY - offsetY + 'px';
 
   balloon.append(label);
   main.append(balloon);
@@ -28,13 +30,28 @@ function createBalloon(e) {
 function addInput(e) {
   const div = e.currentTarget;
   const p = div.firstChild;
-  const input = document.createElement('input');
-  input.classList.add('input');
-  input.setAttribute('value', `${p.innerText}`);
-  input.setAttribute('type', 'text');
-  div.replaceChild(input, p);
-  input.focus();
+  const text = p.innerText;
 
+  p.classList.add('input');
+  p.setAttribute('contentEditable', 'true');
+  p.addEventListener('keydown', (e) => {handleInputKeyDown(e, text)});
+  p.focus();
+}
+
+function handleInputKeyDown(e, content) {
+  const input = e.target;
+
+  if (e.which !== 13 && e.which !== 27) {
+    return;
+  }
+
+  if (e.which === 27 /* escape */) {
+    input.innerText = content;
+  }
+
+    e.preventDefault();
+    input.classList.remove('input');
+    input.blur();
 }
 
 
